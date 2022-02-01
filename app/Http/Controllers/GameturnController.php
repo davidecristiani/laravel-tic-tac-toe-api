fedadd8a883bc667909d6ab4a10b233c58dd96e5
davidecristiani <14573851+davidecristiani@users.noreply.github.com>
2022-02-01 12:41:31 +0700

    newgame endpoint
    newgame endpoint create an empty game turn with a new game_id and return the game_id.

--- app/Http/Controllers/GameturnController.php
+++ app/Http/Controllers/GameturnController.php
@@ -11,11 +11,17 @@ class GameturnController extends Controller
     /**
      * API for creating a new game.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function newgame()
     {
+        
+        $gameturn          = new Gameturn();
+        $gameturn->game_id = rand( 1, pow( 10, 16 ) );
+        $gameturn->saveOrFail();
+        $response_collection = collect( [ 'game_id' => $gameturn->game_id ] );
 
+        return response()->json( $response_collection );
     }
 
     /**
