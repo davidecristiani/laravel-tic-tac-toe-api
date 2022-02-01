d71dbd1a537ebb5769b53c68d41ba0c8762d0514
davidecristiani <14573851+davidecristiani@users.noreply.github.com>
2022-02-01 12:48:29 +0700

    play endpoint params validation
    Basic validation of parameters, player turn, position taken

--- app/Http/Controllers/GameturnController.php
+++ app/Http/Controllers/GameturnController.php
@@ -5,17 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\Gameturn;
 use Illuminate\Http\Request;
 
-class GameturnController extends Controller
-{
+class GameturnController extends Controller {
 
     /**
      * API for creating a new game.
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function newgame()
-    {
-        
+    public function newgame() {
+
         $gameturn          = new Gameturn();
         $gameturn->game_id = rand( 1, pow( 10, 16 ) );
         $gameturn->saveOrFail();
@@ -27,12 +25,33 @@ class GameturnController extends Controller
     /**
      * API for play a game.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function play(Request $request)
-    {
-        //
+    public function play( Request $request ) {
+        try {
+            $request->validate( [
+                'game_id'  => 'required|exists:gameturns',
+                'player'   => 'required|in:' . implode( ',', Gameturn::$players ),
+                'position' => 'required|in:' . implode( ',', Gameturn::$positions ),
+            ] );
+            $last_gameturn = Gameturn::where( 'game_id', $request->input( 'game_id' ) )->latest()->firstOrFail();
+
+        } catch ( \Exception $e ) {
+            return response()->json( [ 'error' => 'Parameters error: ' . $e->getMessage() ], 500 );
+        }
+
+        if ( $request->input( 'player' ) == $last_gameturn->player ) {
+            return response()->json( [ 'error' => 'It is not your turn to play' ], 500 );
+        }
+
+        $position_state = $last_gameturn->{$request->input( 'position' )};
+        if ( $position_state !== 0 ) {
+            return response()->json( [ 'error' => 'The position is already taken by player ' . $position_state ], 500 );
+        }
+
+        //todo check victory
     }
 
 
